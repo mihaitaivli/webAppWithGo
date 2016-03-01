81730c7d85ce8a2754369396c4fe2ead3798a8ce
Ivli Mihaita <mihaitaivli@gmail.com>
2016-03-01 16:25:09 +0200

    New handler

--- src/main/main.go
+++ src/main/main.go
@@ -1,13 +1,28 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
-func main(){
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request){
-			w.Write([]byte("Hello world"))
-			})
-	
+func main() {
+	http.Handle("/", new(MyHandler))
+
 	http.ListenAndServe(":8888", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err == nil {
+		w.Write(data)
+	} else {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
 	}
+}
